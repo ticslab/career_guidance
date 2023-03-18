1bff44694332950a8c19029c775601e73cd6cf6f
ArtemChekalev <76198345+ArtemChekalev@users.noreply.github.com>
2023-03-18 19:53:40 +0300

    Вторая версия парсера
    Добавлена выгрузка в БД, поддержка конфига. Выгрузка групп переделана и теперь реализована с помощью метода execute.

--- vk_parser.py
+++ vk_parser.py
@@ -1,108 +1,137 @@
+import re
+import pymysql
 import pandas as pd
 import asyncio
 import numpy
 import queue
-import vk_api
 import vkbottle
 import time
+from configparser import ConfigParser
 
 
-# path = str(input('Введите путь к csv-файлу с id пользователей:' + '\n'))
-# C:\Users\artem\Downloads\Telegram Desktop\users_info_0.csv
-
-df = pd.read_csv('C:/Users/artem/Downloads/Telegram Desktop/users_info_0.csv', engine='python')
-tokens_ = ['vk1.a.Sjbrtifp5ddYgtNq2DFcnse0WpGyQaE2bhmBkLfg0HPo117t-UT8eHUIQkyfnUvldrWRTyCr36uHLVdrPlxjrHOOItd7ahIjQQWUkWJgLshMcWlMcp5BhGZgTmDilyCCboB0ske_gA4jP2iC2GCZOLS5NWo0nYjcMeltXYr4dTXYPOk3aGlpmj0X_M5Rz2qHcMsut8pfsCwS4G0CaEerHQ',
-           'vk1.a.aPdOAhc4bjJVDm1cxRmH7iSBOvioe188wdaWWkuoda_4Iz7UpHd2SE2JSV9bqDHHVUvcHAvgTvIci1b_kVRQdeOVlJWZDGCmCgYDsZzmeFuLVpfIpT63pWkq-vnRuhwXKb95gq9_thlGjLepT6_q2aW9s9dj1lykyZTjsqsJ9l88zf233v0TOr1cQoFTn3Pqsq0Ov0BpMnwtrVsF1dkxZA']
-
-
-async def get_apis(tokens):
+async def get_apis(tokens: list) -> queue.Queue:
     apis_queue = queue.Queue()
     for token in tokens:
         apis_queue.put(item=vkbottle.API(token=token))
     return apis_queue
 
 
-async def user_info(users: list):
+def create_tables(connection):
+    with connection.cursor() as cur:
+        cur.execute(
+            'CREATE TABLE IF NOT EXISTS USERS(USER_ID VARCHAR(15), IS_CLOSED INT, SEX INT, BDATE_YEAR VARCHAR(4), '
+            'OCCUPATION VARCHAR(1000), CAREER VARCHAR(5000), EDU_INFO INT, EDU_FACULTY VARCHAR(200), '
+            'EDU_GRADUATION INT,PRIMARY KEY (USER_ID));')
+        connection.commit()
+        cur.execute(
+            'CREATE TABLE IF NOT EXISTS USER_GROUPS(USER_ID VARCHAR(15), ACTIVITIES VARCHAR(5000), FOREIGN KEY '
+            '(USER_ID) REFERENCES USERS (USER_ID));')
+        connection.commit()
+
+
+async def user_info(users: list, connection) -> list:
     info = []
     for user in users:
-        user_id = user['id']
-        user_is_closed = 1 if user['is_closed'] else 0
-        user_bdate = user['bdate'][-4:] if user.get('bdate',0) and len(user['bdate']) > 5 else 0
-        user_occupation = user['occupation'] if user.get('occupation', '') else 0
-        user_career = [[job.get('position', 0), job.get('from', 0), job.get('intil', 0)] for job in user['career']] \
-            if user.get('career', []) else 0
-        user_university = 1 if user.get('university', 0) else 0
-        user_education = [user.get('faculty_name', 0), user.get('graduation', 0)]
-        info.append([user_id, user_is_closed, user_bdate, user_occupation, user_career, user_university, user_education[0], user_education[1]])
+        user_id = str(user['id'])
+        user_sex: int = user['sex']
+        user_is_closed: int = 1 if user['is_closed'] else 0
+        user_bdate: str = user['bdate'][-4:] if user.get('bdate', 0) and len(user['bdate']) > 5 else '0'
+        user_occupation: dict or str = user['occupation'] if user.get('occupation', '') else '0'
+        user_career: list or str = [[job.get('position', 0), job.get('from', 0), job.get('until', 0)] for job in
+                                    user['career']] \
+            if user.get('career', []) else '0'
+        user_university: int = 1 if user.get('university', 0) else 0
+        user_education: list = [user.get('faculty_name', '0'), user.get('graduation', 0)]
+        info.append([user_id, user_is_closed, user_sex, user_bdate, user_occupation, user_career, user_university,
+                     user_education[0], user_education[1]])
+        with connection.cursor() as cur:
+            cur.execute(
+                'INSERT USERS(USER_ID, IS_CLOSED, SEX, BDATE_YEAR, OCCUPATION, CAREER, EDU_INFO, EDU_FACULTY,'
+                'EDU_GRADUATION) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s);', (user_id, user_is_closed,
+                                                                        user_sex, user_bdate, str(user_occupation),
+                                                                        str(user_career), user_university,
+                                                                        user_education[0], user_education[1]))
+        connection.commit()
     return info
 
 
-async def group_info(groups: list):
+async def group_info(groups: list, ids: list, connection) -> list:
     result = []
-
     list_of_activities = []
     for group in groups:
         activities = []
-        if type(group) != str:
-            for item in group['items']:
-                activities.append(item.get('activity', 'Private group'))
-            list_of_activities.append(activities)
+        if group:
+            if group['count'] == 0:
+                list_of_activities.append(tuple())
+            else:
+                for item in group['items']:
+                    act = item.get('activity', 'Private group')
+                    meeting = re.search('\d+:\d\d', act)
+                    if 'Этот материал заблокирован' not in act and not meeting:
+                        activities.append(act)
+                    elif meeting:
+                        activities.append('Meeting')
+                list_of_activities.append(activities)
         else:
             list_of_activities.append([])
-    for li in list_of_activities:
+    for i in range(len(list_of_activities)):
         res_dict = {}
-        if li:
-            for act in li:
+        if len(list_of_activities[i]) > 0:
+            for act in list_of_activities[i]:
                 res_dict[act] = res_dict.get(act, 0) + 1
             result.append(res_dict)
+        elif type(list_of_activities[i]) == tuple:
+            result.append({'No groups': 1})
         else:
             result.append({})
+        with connection.cursor() as cur:
+            cur.execute('INSERT USER_GROUPS(USER_ID, ACTIVITIES) VALUES(%s, %s);', (ids[i], str(result[i])))
+        connection.commit()
     return result
 
 
-async def get_info(ids, tokens: list):
+async def get_info(ids: pd.Series, tokens: list, connection) -> None:
     apis = await get_apis(tokens)
     divided_list = numpy.array_split(ids, len(tokens))
-    print(len(divided_list), len(divided_list[0]))
     list_of_users, list_of_groups = [], []
     coros_users = (get_users(ids, False, apis) for ids in divided_list)
     coros_groups = (get_users(ids, True, apis) for ids in divided_list)
+    create_tables(connection)
     for coro in list(coros_users):
         users = await coro
-        info = await user_info(users)
+        info = await user_info(users, connection)
         list_of_users.extend(info)
-    print(list_of_users)
-    user_df = pd.DataFrame(list_of_users, columns=['id','is_closed', 'bdate_year', 'occupation', 'career',
+    user_df = pd.DataFrame(list_of_users, columns=['id', 'is_closed', 'sex', 'bdate_year', 'occupation', 'career',
                                                    'edu_information', 'edu_faculty', 'edu_graduation'])
-    user_df.to_csv('C:/Users/artem/OneDrive/Рабочий стол/data_users.csv', index=False)
-    # for coro in list(coros_groups):
-    #     groups = await coro
-    #     info = await group_info(groups)
-    #     list_of_groups.extend(info)
-    # data = numpy.array([list(ids), list_of_groups]).transpose()
-    # group_df = pd.DataFrame(data, columns=['id', 'activities'])
-    # group_df.to_csv('C:/Users/artem/OneDrive/Рабочий стол/data_groups.csv', index=False)
-    # print('end')
+    user_df.to_csv('data_users.csv', index=False)
+    print('users parsed')
+    for coro in list(coros_groups):
+        groups = await coro
+        info = await group_info(groups, list(ids), connection)
+        list_of_groups.extend(info)
+    data = numpy.array([list(ids), list_of_groups]).transpose()
+    group_df = pd.DataFrame(data, columns=['id', 'activities'])
+    group_df.to_csv('data_groups.csv', index=False)
+    print('groups parsed')
 
 
 async def get_users(user_ids: list, one_by_one: bool, apis) -> list:
     list_of_users = []
     api_requests = queue.Queue()
     if one_by_one:
-        # Здесь мы хотим узнавать информацию про группы
-        user_ids_list = user_list_handler(user_ids, 1)
-        for i in range(len(user_ids_list)):
-            api_requests.put(('groups.get', {'user_id': user_ids_list[i], 'fields': 'activity', 'extended': 1}))
+        user_ids_list = user_list_handler(user_ids, 25)
+        for li in user_ids_list:
+            api_requests.put(li)
     else:
-        user_ids_list = user_list_handler(user_ids, len(user_ids)//3)
+        user_ids_list = user_list_handler(user_ids, len(user_ids) // 3)
         for i in range(len(user_ids_list)):
             api_requests.put(('users.get',
                               {'user_id': user_ids_list[i], 'fields': 'sex, bdate, occupation, education, career'}))
     while not api_requests.empty():
         api_request = api_requests.get()
-        response = await response_executor(api_request, apis)
+        response = await response_executor(api_request, apis, one_by_one)
         if one_by_one:
-            list_of_users.append(response['response'])
+            list_of_users.extend(response['response'])
         else:
             list_of_users.extend(response['response'])
     return list_of_users
@@ -110,20 +139,14 @@ async def get_users(user_ids: list, one_by_one: bool, apis) -> list:
 
 def user_list_handler(user_ids: list, num: int) -> list:
     if num == 1:
-        return [str(id) for id in user_ids]
+        return [str(user_id) for user_id in user_ids]
     else:
         user_list = []
-        # а если len(user_ids)//num == 0? Тут либо правильно подобрать
-        # num, либо отдельно обрабатывать случай равенства 0
-        for i in range(len(user_ids)//num):
+        for i in range(len(user_ids) // num):
             user_list.append(','.join(user_ids[i * num: (i + 1) * num]))
         if len(user_ids) % num != 0:
-            user_list.append(','.join(user_ids[(-1)*(len(user_ids) % num):]))
+            user_list.append(','.join(user_ids[(-1) * (len(user_ids) % num):]))
         return user_list
-
-
-async def vk_error_handler(apis, api):
-    asyncio.create_task(put_with_timeout(apis, api, 600))
 
 
 async def put_with_timeout(apis, api, t):
@@ -131,23 +154,81 @@ async def put_with_timeout(apis, api, t):
     apis.put(api)
 
 
-async def response_executor(api_request: tuple, apis: queue.Queue):
+async def response_executor(api_request: tuple or str, apis: queue.Queue, one_by_one: bool) -> dict:
     api = None
     while True:
-        try:
-            request, dictionary = api_request
+        if not one_by_one:
+            try:
+                request, dictionary = api_request
+                api = apis.get()
+                response = await api.request(request, dictionary)
+                await put_with_timeout(apis, api, 0.34)
+                return response
+            except vkbottle.VKAPIError as error:
+                print(error)
+                if error.code == 29:
+                    task = asyncio.create_task(put_with_timeout(apis, api, 600))
+                    await task
+                else:
+                    await put_with_timeout(apis, api, 0.34)
+                    break
+        else:
             api = apis.get()
-            response = await api.request(request, dictionary)
+            response = await api.execute(
+                f"var u = [{str(api_request)}];"
+                "var i = 0;"
+                "var res = [];"
+                "while (i < u.length){"
+                "var api = API.groups.get({'user_id': u[i], 'fields':'activity', 'extended': 1});"
+                "if (api){"
+                "res.push(api);"
+                "}"
+                "else{"
+                "res.push([]);"
+                "}"
+                "i = i + 1;"
+                "};"
+                "return res;"
+            )
             await put_with_timeout(apis, api, 0.34)
             return response
-        except vkbottle.VKAPIError as error:
-            if error.code == 29:
-                await vk_error_handler(apis, api)
-            else:
-                await put_with_timeout(apis, api, 0.34)
-                return {'response': 'Error'}
-
-asyncio.run(get_info(df['id'].astype('str')[:500], tokens_))
 
 
+def read_config(filename):
+    parser = ConfigParser()
+    parser.read(filename)
+    res = []
+    for section in parser.sections():
+        items = parser.items(section)
+        li_section = []
+        for item in items:
+            li_section.append(str(item[1]))
+        res.append(li_section)
+    return res
 
+
+async def main(filename, path):
+    database, tokens = read_config(filename)
+    connection = None
+    try:
+        connection = pymysql.connect(
+            host=database[0],
+            user=database[1],
+            password=database[2],
+            db=database[3],
+            charset=database[4]
+        )
+        print("Connection to DB successful")
+    except pymysql.Error as error:
+        print(f'An error occurred: {error}')
+    if connection is not None:
+        ids_file = pd.read_csv(path, engine='python')
+        task = asyncio.create_task(get_info(ids_file['id'].astype('str')[:500], tokens, connection))
+        await task
+
+
+# 'C:/Users/artem/PycharmProjects/pd/venv/config.ini'
+# 'C:/Users/artem/Downloads/Telegram Desktop/users_info_0.csv'
+config_path = input('Enter config file path: \n')
+ids_path = input('Enter ids file path: \n')
+asyncio.run(main(config_path, ids_path))
